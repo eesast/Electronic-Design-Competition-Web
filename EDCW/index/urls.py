6398be711a0d4baa2f53c9bcf0964e7fd4283519
alxl <mildwindyu@hotmail.com>
2016-08-20 15:07:10 +0800

    add Welcome page

--- EDCW/index/urls.py
+++ EDCW/index/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'index'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.welcome, name='welcome'),
+    url(r'^index', views.index, name='index'),
 ]
--- EDCW/index/views.py
+++ EDCW/index/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
 
     return render(request, 'index/index.html')
+
+def welcome(request):
+
+    return render(request, 'index/Welcome.html')
