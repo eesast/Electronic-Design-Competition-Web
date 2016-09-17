def85ba03e5ba9cba6834a93b7a68c5d46948a02
Will Zong <zongzefang@hotmail.com>
2016-09-17 22:36:01 +0800

    postforumfinished

--- EDCW/community/views.py
+++ EDCW/community/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, render_to_response,HttpResponseRedirect
 from community import forms
-from community.models import Post,PostFile,Comment,Category
+from community.models import Post,PostFile,Comment
 from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
 from django.contrib.auth import authenticate, login
 from community.forms import PostForm,CommentForm
 
+Category_List=(
+		('1','赛事公告'),
+		('2','技术讨论'),
+		('3','各路水贴'),
+		)
+
 def community_content(request,id):
 	post = Post.objects.get(id=int(id))
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		delcomment = Comment.objects.get(id=int(delete_id))
+		delcomment.post.replycount-=1
+		delcomment.post.save()
+		delcomment.delete()
+	except Exception:
+		pass
 	if 	request.user.is_authenticated() :
 		if request.method=='POST':
 			form = CommentForm(request.POST)
@@ -15,6 +30,7 @@ def community_content(request,id):
 				comment = Comment(content=cd['comment'],post = Post.objects.get(id=int(id)),replier=request.user)
 				comment.save()
 				comment.post.replycount+=1
+				comment.post.save()
 			else:
 				form = CommentForm()
 		commentlist = post.p_comment.all()		
@@ -26,17 +42,18 @@ def community_content(request,id):
 
 
 def community_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            print (2)
-            cd = form.cleaned_data
-            post = Post(title=cd['title'],content=cd['body'],sender=request.user,summary=cd['outline'],category=cd['category'])
-            post.save()
-            return HttpResponseRedirect('/community_index')
-    else:
-        form = PostForm()
-    return render(request,'community_create.html', {'form': form})
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		print(form.errors)
+		if form.is_valid():
+			print (2)
+			cd = form.cleaned_data
+			post = Post(title=cd['title'],content=cd['body'],sender=request.user,summary=cd['outline'],category=cd['category'])
+			post.save()
+			return HttpResponseRedirect('/community_index')
+	else:
+		form = PostForm()
+	return render(request,'community_create.html', {'form': form})
         
 
 
@@ -45,6 +62,13 @@ def community_create(request):
 def community_index(request,page=1):
 	post_list = Post.objects.all()
 	paginator = Paginator(post_list,5)
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		post = Post.objects.get(id=int(delete_id))
+		post.delete()
+	except Exception:
+		pass
 	try:
 		page = int (request.GET.get('page','1'))
 	except ValueError:
@@ -67,17 +91,133 @@ def community_index(request,page=1):
 	else:
 		return render(request,'community_index.html',{"posts":posts,"pagerange":pagerange,})
 
-
-
-def delpost(request,id):
-	post = Post.objects.get(id=int(id))
-	post.delete()
-	return Httpresponse()
-	
-def delcomment(request,id):
-	comment = Comment.objects.get(id=int(id))
-	comment.delete()
-	return Httpresponse()
+def community_index_tongzhi(request,page=1):
+	post_list = Post.objects.filter(category='1')
+	paginator = Paginator(post_list,5)
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		post = Post.objects.get(id=int(delete_id))
+		post.delete()
+	except Exception:
+		pass
+	try:
+		page = int (request.GET.get('page','1'))
+	except ValueError:
+		page = 1
+	try:
+		posts = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		posts = paginator.page(paginator.num_pages)
+	for i in range(page,page+3):
+		if i > paginator.num_pages:
+			break
+		maxpage = i
+	for i in range(page,page-3,-1):
+		if i < 1:
+			break
+		minpage = i
+	pagerange=range(minpage,maxpage+1)
+	if request.user.is_authenticated():
+		return render(request,'community_index_after_login.html',{"posts":posts,"pagerange":pagerange,})
+	else:
+		return render(request,'community_index.html',{"posts":posts,"pagerange":pagerange,})
+		
+def community_index_jishu(request,page=1):
+	post_list = Post.objects.filter(category='2')
+	paginator = Paginator(post_list,5)
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		post = Post.objects.get(id=int(delete_id))
+		post.delete()
+	except Exception:
+		pass
+	try:
+		page = int (request.GET.get('page','1'))
+	except ValueError:
+		page = 1
+	try:
+		posts = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		posts = paginator.page(paginator.num_pages)
+	for i in range(page,page+3):
+		if i > paginator.num_pages:
+			break
+		maxpage = i
+	for i in range(page,page-3,-1):
+		if i < 1:
+			break
+		minpage = i
+	pagerange=range(minpage,maxpage+1)
+	if request.user.is_authenticated():
+		return render(request,'community_index_after_login.html',{"posts":posts,"pagerange":pagerange,})
+	else:
+		return render(request,'community_index.html',{"posts":posts,"pagerange":pagerange,})
+		
+def community_index_shuitie(request,page=1):
+	post_list = Post.objects.filter(category='3')
+	paginator = Paginator(post_list,5)
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		post = Post.objects.get(id=int(delete_id))
+		post.delete()
+	except Exception:
+		pass
+	try:
+		page = int (request.GET.get('page','1'))
+	except ValueError:
+		page = 1
+	try:
+		posts = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		posts = paginator.page(paginator.num_pages)
+	for i in range(page,page+3):
+		if i > paginator.num_pages:
+			break
+		maxpage = i
+	for i in range(page,page-3,-1):
+		if i < 1:
+			break
+		minpage = i
+	pagerange=range(minpage,maxpage+1)
+	if request.user.is_authenticated():
+		return render(request,'community_index_after_login.html',{"posts":posts,"pagerange":pagerange,})
+	else:
+		return render(request,'community_index.html',{"posts":posts,"pagerange":pagerange,})
+		
+def community_index_mypost(request,page=1):
+	post_list = Post.objects.filter(sender=request.user)
+	paginator = Paginator(post_list,5)
+	delete_id = None
+	delete_id = request.POST.get('delete_id','')
+	try:
+		post = Post.objects.get(id=int(delete_id))
+		post.delete()
+	except Exception:
+		pass
+	try:
+		page = int (request.GET.get('page','1'))
+	except ValueError:
+		page = 1
+	try:
+		posts = paginator.page(page)
+	except (EmptyPage,InvalidPage):
+		posts = paginator.page(paginator.num_pages)
+	for i in range(page,page+3):
+		if i > paginator.num_pages:
+			break
+		maxpage = i
+	for i in range(page,page-3,-1):
+		if i < 1:
+			break
+		minpage = i
+	pagerange=range(minpage,maxpage+1)
+	if request.user.is_authenticated():
+		return render(request,'community_index_after_login.html',{"posts":posts,"pagerange":pagerange,})
+	else:
+		return render(request,'community_index.html',{"posts":posts,"pagerange":pagerange,})
 
 
 
